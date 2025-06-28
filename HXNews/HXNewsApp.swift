3742e37e5859045cd0eb195fdbffdfa4d855b8de
theAyush-191 <ayush191.znr@gmail.com>
2025-06-28 22:25:00 +0530

    Initial Commit

---
+++ HXNews/HXNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HXNewsApp.swift
+//  HXNews
+//
+//  Created by Ayush Singh on 28/06/25.
+//
+
+import SwiftUI
+
+@main
+struct HXNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
